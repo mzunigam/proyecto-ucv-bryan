36508e41e961114c9134430eb6a96a4f3b530ac7
Tomoyo01~ <dick.brayan.tk@gmail.com>
2023-12-07 00:52:44 -0500

    mover a la pagina principal

--- Js/prevenir.js
+++ Js/prevenir.js
@@ -2,11 +2,12 @@ window.addEventListener('load', () => {
     const usuarioSesion = sessionStorage.getItem('usuario') || null;
     const usuario = JSON.parse(usuarioSesion);
     console.log(usuario)
+    seguridad(usuario.idtipo_usuario)
 
     if(!usuario){
         window.location.href = './login.html'
         document.getElementById('perfil').style.display = 'none';
-        
+    
     }else{
         document.getElementById('nombreUsuario').textContent =  usuario.usuario;
         document.getElementById('iniciarSesion').style.display = 'none';
---
+++ Js/principal.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    verindex();
+});
+const verindex = () => {
+    console.log('Me ejecute')
+    window.location.replace('../vistas/index.html');
+
+}
